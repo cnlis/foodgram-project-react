67098e3d7074c645f0348e7c52d74c7587b4d61f
Kirill Lisitsynskiy <kirill.lisitsynskiy@gmail.com>
2022-04-03 22:50:24 +0800

    Fix import

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator, \
-    MaxLengthValidator
+from django.core.validators import (MaxLengthValidator, MaxValueValidator,
+                                    MinValueValidator)
 from django.db import models
 from django.db.models import UniqueConstraint
 from django.utils.translation import gettext_lazy as _
